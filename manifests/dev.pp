1dc5fe225b86e1b02280eedfebb91b861d76c8be
alan.hughes <alan.hughes@qa.com>
2016-09-16 18:06:28 +0100

    fix lint error

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -2,7 +2,7 @@ class apache::dev {
 
   if ! defined(Class['apache']) {
     fail('You must include the apache base class before using any apache defined resources')
-  
+  }
 
   $packages == $::apache::dev_packages
   if $packages { # FreeBSD doesn't have dev packages to install
